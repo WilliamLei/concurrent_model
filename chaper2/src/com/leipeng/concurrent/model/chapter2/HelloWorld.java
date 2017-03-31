7fd472725afb1a88125b563447820744149d2f43
leipeng03 <leipeng03@meituan.com>
2017-03-31 20:18:24 +0800

    init

---
+++ chaper2/src/com/leipeng/concurrent/model/chapter2/HelloWorld.java
@@ -0,0 +1,19 @@
+package com.leipeng.concurrent.model.chapter2;
+
+/**
+ * Created by leipeng on 31/03/2017.
+ */
+public class HelloWorld {
+    public static void main(String[] args) throws Exception {
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                System.out.println("Hello from new thread");
+            }
+        };
+        thread.start();
+        Thread.yield();
+        System.out.println("Hello from main thread");
+        thread.join();
+    }
+}
